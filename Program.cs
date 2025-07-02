1ebca88ea564cfa6b17192c591762f16711ea62e
user <ivasiuk.andrii@chnu.edu.ua>
2025-07-02 15:50:51 +0300

    service

--- Program.cs
+++ Program.cs
@@ -4,22 +4,32 @@ using ServiceApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Налаштування контексту бази даних
 builder.Services.AddDbContext<ServiceDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 36))));
 
 builder.Services.AddControllers();
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Service API", Version = "v1" });
 });
 
+// AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Централізована обробка помилок
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error"); // або кастомний middleware
+    app.UseHsts();
+}
+else
 {
     app.UseSwagger();
     app.UseSwaggerUI();
